78070bbeaabfbc0bf5496feb1f42a4f269cade33
FionaGachuuri <fionairuka@gmail.com>
2025-04-17 21:07:29 +0300

    Numerical python

---
+++ python_libraries/README.md
@@ -0,0 +1,7 @@
+Practice Task (NumPy)
+
+Create an array with numbers 10 to 50.
+
+Find the maximum and minimum values.
+
+Multiply all elements by 3.
---
+++ python_libraries/numpyeg.py
@@ -0,0 +1,16 @@
+import  numpy as np
+# create an array with numbers 10 to 50
+arr = np.arange(10, 51)
+print("Array:", arr)
+
+# Maximum of the values
+max_value = np.max(arr)
+print("Maximum value:", max_value)
+
+# Minimum of the values
+min_value = np.min(arr)
+print("Minimum value:", min_value)
+
+# Multiply all elements by 3
+arr_multiplied = arr * 3
+print("Array multiplied by 3:", arr_multiplied)
